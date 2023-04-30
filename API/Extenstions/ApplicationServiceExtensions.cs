0ec31c10020aed876bda6dcfbb2c64f79039ed6c
Era Graiçevci <93553954+Eragraicevci@users.noreply.github.com>
2023-04-30 04:52:48 +0200

    Photo Service

--- API/Extenstions/ApplicationServiceExtensions.cs
+++ API/Extenstions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +21,9 @@ namespace API.Extenstions
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+            services.AddScoped<IPhotoService, PhotoService>();
+
             return services;
         }
     }
