26b4ab13a9022da6151d9329152b1bfec32213b5
Era Graiçevci <93553954+Eragraicevci@users.noreply.github.com>
2023-05-10 00:21:54 +0200

    Getting the messages from the Repo & Deleting them

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -36,63 +36,44 @@ namespace API.Data
 
         public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
         {
-            var query = _context.Messages.OrderByDescending(x => x.MessageSent).AsQueryable();
+            var query = _context.Messages
+                .OrderByDescending(x => x.MessageSent)
+                .AsQueryable();
 
             query = messageParams.Container switch
             {
-                "Inbox"
-                    => query.Where(
-                        u =>
-                            u.RecipientUsername == messageParams.Username
-                            && u.RecipientDeleted == false
-                    ),
-                "Outbox"
-                    => query.Where(
-                        u => u.SenderUsername == messageParams.Username && u.SenderDeleted == false
-                    ),
-                _
-                    => query.Where(
-                        u =>
-                            u.RecipientUsername == messageParams.Username
-                            && u.RecipientDeleted == false
-                            && u.DateRead == null
-                    )
+                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username 
+                    && u.RecipientDeleted == false),
+                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username 
+                    && u.SenderDeleted == false),
+                _ => query.Where(u => u.RecipientUsername == messageParams.Username 
+                    && u.RecipientDeleted == false && u.DateRead == null)
             };
 
             var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
 
-            return await PagedList<MessageDto>.CreateAsync(
-                messages,
-                messageParams.PageNumber,
-                messageParams.PageSize
-            );
+            return await PagedList<MessageDto>
+                .CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
         }
 
-        public async Task<IEnumerable<MessageDto>> GetMessageThread(
-            string currentUserName,
-            string recipientUserName
-        )
+
+
+         public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUserName, string recipientUserName)
         {
             var messages = await _context.Messages
-                .Include(u => u.Sender)
-                .ThenInclude(p => p.Photos)
-                .Include(u => u.Recipient)
-                .ThenInclude(p => p.Photos)
+                .Include(u => u.Sender).ThenInclude(p => p.Photos)
+                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
                 .Where(
-                    m =>
-                        m.RecipientUsername == currentUserName
-                            && m.RecipientDeleted == false
-                            && m.SenderUsername == recipientUserName
-                        || m.RecipientUsername == recipientUserName
-                            && m.SenderDeleted == false
-                            && m.SenderUsername == currentUserName
+                    m => m.RecipientUsername == currentUserName && m.RecipientDeleted == false &&
+                    m.SenderUsername == recipientUserName ||
+                    m.RecipientUsername == recipientUserName && m.SenderDeleted == false &&
+                    m.SenderUsername == currentUserName
                 )
                 .OrderBy(m => m.MessageSent)
                 .ToListAsync();
 
-            var unreadMessages = messages
-                .Where(m => m.DateRead == null && m.RecipientUsername == currentUserName)
-                .ToList();
+            var unreadMessages = messages.Where(m => m.DateRead == null 
+                && m.RecipientUsername == currentUserName).ToList();
 
             if (unreadMessages.Any())
             {
--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -18,4 +18,19 @@ export class MessageService {
     return getPaginatedResult<Message[]>(this.baseUrl + 'messages', params, this.http);
   }
 
+  getMessageThread(username: string) {
+    return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
+  }
+
+  sendMessage(username: string, content: string) {
+    return this.http.post<Message>(this.baseUrl + 'messages', 
+      {recipientUsername: username, content});
+  }
+
+  deleteMessage(id: number) {
+    console.log("Deleting message with ID: ", id);
+    return this.http.delete(this.baseUrl + 'messages/' + id);
+  }
+  
+  
 }
